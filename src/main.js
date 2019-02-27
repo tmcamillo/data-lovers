11b7d425c0e2712504c01da7839184bb5e963c24
tmcamillo <imeio.tmc@gmail.com>
2019-02-27 18:40:17 -0300

    horário convertido para dia da semana/mes/dia/ano

--- src/main.js
+++ src/main.js
@@ -6,32 +6,17 @@ function getAllNews(){
     return STEAM["appnews"]["newsitems"]; 
 };
 
-// function getNFixDate(){
-//     let date = new Date(STEAM["appnews"]["newsitems"]["date"]*1000);
-//     console.log(date);
-//     //let day = date.getDate().toString();
-    
-//     // let month = date.getMonth().toString();
-//     // let year = date.getFullYear().toString();
-//     // let dateFixed = day + month + year;
-//     // return dateFixed;
-// };
-
-//console.log(getNFixDate());
-
 function displayAllNews(){
-    let newsDiv = document.getElementById("news-list");
+    let newsDiv = document.querySelector("#news-container");
     newsDiv.innerHTML = `${getAllNews().map((materia) => `
-    <div class="news-style"> 
+    <article class="news-style"> 
         <h2 class="news-title">${materia["title"]} </h2>
-        <h3 class="news-date">${new Date(materia["date"]*1000)} </h3>
+        <h3 class="news-date">${new Date(materia["date"]*1000).toDateString()} </h3>
         <p class="news-text"> ${materia["contents"]} </p>
-	</div>
+	</article>
   `).join("")}
   `
 }
-
-
 
 //let dropMenu = document.getElementById("drop-menu-categorias");
 //dropMenu.addEventListener("change", loadOptionsMenu);
