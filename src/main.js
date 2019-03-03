8e5709fb5d96c3d0238625fb2fb1fe838e1813a1
tmcamillo <imeio.tmc@gmail.com>
2019-03-03 19:50:53 -0300

    add função para carregar por data e corrigido select data no html

--- src/main.js
+++ src/main.js
@@ -2,24 +2,38 @@ window.onload = function() {
     displayAllNews();
 };
 
-function getAllNews(){
-    return STEAM["appnews"]["newsitems"]; 
+function getAllObj(){
+    return STEAM["appnews"]["newsitems"];  
 };
 
 function displayAllNews(){
     let newsDiv = document.querySelector("#news-container");
-    newsDiv.innerHTML = `${getAllNews().map((materia) => `
-    <article class="news-style"> 
+    newsDiv.innerHTML = `${getAllObj().map((materia) => `
+    <div class="news-style"> 
         <h2 class="news-title">${materia["title"]} </h2>
         <h3 class="news-date">${new Date(materia["date"]*1000).toDateString()} </h3>
         <p class="news-text"> ${materia["contents"]} </p>
-	</article>
+	</div>
   `).join("")}
   `
-}
+};
 
-//let dropMenu = document.getElementById("drop-menu-categorias");
-//dropMenu.addEventListener("change", loadOptionsMenu);
-//
-// 
+const dateObj = getAllObj().map(word => word.date);
+console.log(dateObj);
 
+
+let dropListDate = document.querySelector(".drop-list-date");
+dropListDate.addEventListener("change", loadByDate);
+
+function loadByDate() {
+    let dateChosen = parseInt(dropListDate.value);
+    let newsDiv = document.querySelector(".news-style");
+    //console.log("escolhido", typeof(dateChosen));
+    for (data of dateObj) {
+        //console.log("iteraçao", typeof(data));
+        if (dateChosen === data){ 
+    //        console.log("chegou")
+
+        }
+    }
+};
