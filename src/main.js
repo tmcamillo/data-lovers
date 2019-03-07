a6afffd7fb1b480045666b8d183b56725ff5b996
ElenAlmeida <elenalmeida@outlook.com>
2019-03-07 19:44:08 -0300

    Troca da função de filtro pelo método filter.

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const newsChannels = document.querySelector('#newsChannels');
 const dropListDate = document.querySelector(".drop-list-date");
 const sortList = document.querySelector(".sort-list");
 const uniqueDates = [...new Set(allNews.map(word => word.date))];
+const totalOfNews= document.querySelector(".total-sum-news");
 
 
 let feedDropListDates = () => {
@@ -23,7 +24,7 @@ let feedDropListDates = () => {
     
 let formatDate = (date) => {
 	let newDate = new Date(date*1000);
-	let fixed = `${newDate.getFullYear()}-${newDate.getMonth() + 1 }-${newDate.getDate()} ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds() < 10 ? '0' + newDate.getSeconds(): newDate.getSeconds()}`;
+	let fixed = `${newDate.getFullYear()}-${newDate.getMonth() + 1 }-${newDate.getDate()}+ ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds() < 10 ? '0' + newDate.getSeconds(): newDate.getSeconds()}`;
 	return fixed;
 };
 
@@ -35,7 +36,9 @@ let displayNews = (filteredNews) => {
         <p class="news-text"> ${materia["contents"]} </p>
 	</div>
   `).join("")}
-  `
+  ` 
+  let sumFilteredNews = Object.keys(filteredNews).length;
+  totalOfNews.innerHTML = `<p>Foram encontrados um total de ${sumFilteredNews} notícias para o seu filtro.</p>`
 };
 
 dropListDate.addEventListener("change", loadByDate);
@@ -55,32 +58,15 @@ function loadByDate() {
 
 newsChannels.addEventListener('change', filter); 
 
-function filter(){ 
-  let newsDiv = document.querySelector("#news-container");  
-  newsDiv.innerHTML='';
-  let channel = newsChannels.value;  
-  let filteringChannel = STEAM.appnews['newsitems'];  
-  for (let subjectMatter of filteringChannel){    
-    if(channel===subjectMatter.feedname){     
-      let title=subjectMatter.title
-      let date= new Date((subjectMatter.date)*1000).toDateString()
-      let contents=subjectMatter.contents
-      print( title, date, contents);
-    }
-  }
-} 
-
-function print(title, date, contents){    
-    let result = document.createElement('div') 
-    let template= `
-    <h2>${title}</h2>
-    <p>${date}</p>
-    <p>${contents}</p>      
-    ` 
-    result.innerHTML=template;     
-    newsDiv.appendChild(result)
+function filter(){
+  let channel = newsChannels.value; 
+  if(channel){
+    let filteredNews = allNews.filter((materia) => {
+      return materia.feedname === channel
+  });
+    displayNews(filteredNews);
+  }  
 }
-
 
 sortList.addEventListener("change", sortedByDate);
 
@@ -95,3 +81,5 @@ function sortedByDate() {
 		displayNews(downward);
 	}
 };
+
+
