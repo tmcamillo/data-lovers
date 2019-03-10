1a5ed591bc75166d2a9337de68e281b6d6512be6
tmcamillo <imeio.tmc@gmail.com>
2019-03-10 18:28:34 -0300

    add uma cópia do objeto para incluir data2, add função para contar ocorrencia de máterias por canal e metódo para contar resultado de matérias de acordo com filtro; add condição para suprimir o select de ordenar e botão para limpar filtros.

--- src/main.js
+++ src/main.js
@@ -1,29 +1,30 @@
 window.onload = function() {
     displayNews(allNews);
-    feedDropListDates();
+	feedDropListDates();	
 };
 
 let formatDate = (date) => {
 	let newDate = new Date(date*1000);
-	let fixed = `${newDate.getFullYear()}-${newDate.getMonth() + 1 }-${newDate.getDate()}`;
+	let fixed = `${newDate.getDate()}-${newDate.getMonth() + 1 }-${newDate.getFullYear()}`;
 	return fixed;
 };
 
-let originalObj = STEAM["appnews"]["newsitems"]; 
-const allNews =  originalObj.map(item => {  return {...item, date2: formatDate(item.date)}  })
-const newsDiv = document.querySelector("#news-container");  
-const totalOfNews = document.querySelector(".total-sum-news");
+const originalObj = STEAM['appnews']['newsitems']; 
+let allNews =  originalObj.map(item => {  return {...item, date2: formatDate(item.date)}  })
+let uniqueDates = [...new Set(allNews.map(item => item.date2))];
+let uniqueChannels = [...new Set(allNews.map(item => item.feedname))];
+const newsDiv = document.querySelector('#news-container');  
+const totalOfNews = document.querySelector('.total-sum-news');
 const newsChannels = document.querySelector('#newsChannels');
-const dropListDate = document.querySelector(".drop-list-date");
-const sortList = document.querySelector(".sort-list");
-const uniqueDates = [...new Set(allNews.map(word => word.date2))];
-
+const dropListDate = document.querySelector('.drop-list-date');
+const sortList = document.querySelector('.sort-list');
+const buttonHome=document.querySelector('.btn');
 
 let feedDropListDates = () => {
-    for (date of uniqueDates){
-        let option = document.createElement("option");
-        option.setAttribute("value", date);
-        option.setAttribute("class", "drop-option-date");
+    for (let date of uniqueDates){
+        let option = document.createElement('option');
+        option.setAttribute('value', date);
+        option.setAttribute('class', 'drop-option-date');
         option.textContent = date;
         dropListDate.appendChild(option);
     }
@@ -32,54 +33,85 @@ let feedDropListDates = () => {
 let displayNews = (filteredNews) => {
     newsDiv.innerHTML = `${filteredNews.map((materia) => `
     <div class="news-style"> 
-        <h2 class="news-title">${materia["title"]} </h2>
-        <h3 class="news-date">${formatDate(materia["date"])} </h3>
-        <p class="news-text"> ${materia["contents"]} </p>
+        <h2 class="news-style">${materia['title']} </h2>
+        <h3 class="news-style">${formatDate(materia['date'])} </h3>
+        <p class="news-style"> ${materia['contents']} </p>
 	</div>
-  `).join("")}
+  `).join('')}
   `
     let sumFilteredNews = Object.keys(filteredNews).length;
     totalOfNews.innerHTML = `<p>Foi encontrado um total de ${sumFilteredNews} notícias para seu filtro</p>`  
 }; 
 
-dropListDate.addEventListener("change", loadByDate);
-
+dropListDate.addEventListener('change', loadByDate);
 function loadByDate() {
-    let dateChosen = dropListDate.value;
-    if (dateChosen){
-        let filteredNews = allNews.filter((materia) => {
-			  return materia.date2 === dateChosen
-        });
-      displayNews(filteredNews);
-		
-    }
-    else {
-        displayNews(allNews);
-	}
+	let dateChosen = dropListDate.value;
+	let filteredNews = allNews.filter((materia) => { return materia.date2 === dateChosen; });
+    displayNews(filteredNews);
+    if(dateChosen != 0) {
+      sortList.disabled = 'disabled';
+      sortList.style.display = 'none';
+    }   
 };
 
 newsChannels.addEventListener('change', filter); 
+function filter() {
+	let channel = newsChannels.value; 
+	let filtered = allNews.filter((materia) => { return materia.feedname === channel; });
+	displayNews(filtered);
+    if(channel != 0) {
+      sortList.disabled = 'disabled';
+      sortList.style.display = 'none';
+    }
+};
 
-function filter(){
-  let channel = newsChannels.value; 
-  if(channel){
-    let filteredNews = allNews.filter((materia) => {
-      return materia.feedname === channel
-  });
-    displayNews(filteredNews);
-  }  
-}
+buttonHome.addEventListener('click', home);
+function home() {
+	let channel = newsChannels.value;
+	let dateChosen = parseInt(dropListDate.value);
+	if (channel != 0 || dateChosen != 0 ){
+		channel = 0   
+		dateChosen = 0
+		sortList.disabled = '';
+		sortList.style.display = 'block';
+	}
+	displayNews(allNews); 
+};
 
-sortList.addEventListener("change", sortedByDate);
-
+sortList.addEventListener('change', sortedByDate);
 function sortedByDate() {
 	let sortChosen = sortList.value;
-	if (sortChosen === "oldest") {
+	if (sortChosen === 'oldest') {
 		let upward = allNews.sort((a,b) => new Date(a.date) - new Date(b.date));
 		displayNews(upward);
 	}
-	else if (sortChosen === "latest") {
+	else if (sortChosen === 'latest') {
 		let downward = allNews.sort((a,b) => new Date(b.date) - new Date(a.date));
 		displayNews(downward);
 	}
+};
+
+let countOfOcurrency = allNews.reduce(function(sums,entry){ sums[entry.feedname] = (sums[entry.feedname] || 0) + 1;
+	return sums;
+  },{});
+
+let channelKey = Object.keys(countOfOcurrency);
+let channelValue = Object.values(countOfOcurrency);
+let arr = [['título', 'quantidade']];
+
+for (let index in channelKey){
+	arr.push([channelKey[index].toUpperCase(),channelValue[index]])
+  }
+
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+	var data = google.visualization.arrayToDataTable(arr)
+	var options = {
+		title: 'Notícia por Canais',
+		is3D: true,
+		backgroundColor: 'transparent'
+	};
+	var chart = new google.visualization.PieChart(document.getElementById('piechart-3d'));
+	chart.draw(data, options);
 };
